559102d6f2025a2566fd9c34eab4c1e2e2123ed6
zoemanning <irlandazoe.mm@gmail.com>
2022-04-15 09:15:39 -0400

    user repo, services, service impl

--- src/main/java/domain/user/repo/UserRepo.java
+++ src/main/java/domain/user/repo/UserRepo.java
@@ -1,4 +1,10 @@
 package domain.user.repo;
 
-public interface UserRepo {
+import domain.user.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepo extends CrudRepository<User,Long> {
+    Optional<User> findByLastName(String lastName);
 }
--- src/main/java/domain/user/services/UserServiceImpl.java
+++ src/main/java/domain/user/services/UserServiceImpl.java
@@ -1,4 +1,50 @@
 package domain.user.services;
 
-public class UserServiceImpl {
+import domain.user.exception.UserNotFoundException;
+import domain.user.model.User;
+import domain.user.repo.UserRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService{
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+    private UserService userService;
+    private UserRepo userRepo;
+
+    @Autowired
+    public UserServiceImpl(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public User create(User user) {
+        return userRepo.save(user);
+    }
+
+    @Override
+    public User findById(Long id) {
+        Optional<User> userOptional = userRepo.findById(id);
+        if(userOptional.isEmpty())
+            throw new UserNotFoundException("User not found");
+        return userOptional.get();
+    }
+
+    @Override
+    public Iterable<User> findAll() {
+        return userRepo.findAll();
+    }
+
+    @Override
+    public void delete(Long id) {
+        Optional<User> userOptional = userRepo.findById(id);
+        if(userOptional.isEmpty())
+            throw  new UserNotFoundException("User not found");
+        User userToRemove = userOptional.get();
+        userRepo.delete(userToRemove);
+    }
 }
